57d503460f8b370a68a7d36a31081b58d8366892
Romeyus <marco.possamai@proton.me>
2026-08-20 15:24:06 +0100

    feat: add and_then method (tested)

--- src/rm/_maybe.py
+++ src/rm/_maybe.py
@@ -1,3 +1,4 @@
+from collections.abc import Callable
 from dataclasses import dataclass
 from typing import Any, ClassVar, cast
 
@@ -11,10 +12,17 @@ class Some[T]:
     def and_[U](self, maybe: Maybe[U]) -> Maybe[U]:
         return maybe
 
+    def and_then[U](self, func: Callable[[T], Maybe[U]]) -> Maybe[U]:
+        return func(self.value)
+
 
 class Nothing[T = Any]:
     value: ClassVar[None] = None
 
     @classmethod
     def and_[U](cls, maybe: Maybe[U]) -> Maybe[U]:
+        return cast(Maybe[U], cls)
+
+    @classmethod
+    def and_then[U](cls, func: Callable[[T], Maybe[U]]) -> Maybe[U]:
         return cast(Maybe[U], cls)
--- tests/test_maybe.py
+++ tests/test_maybe.py
@@ -19,3 +19,14 @@ def test_and_() -> None:
 
     assert rm.Nothing.and_(rm.Some(0)) == rm.Nothing
     assert rm.Nothing.and_(rm.Nothing) == rm.Nothing
+
+
+def test_and_then() -> None:
+    """Should return result of `func` if `Some`; else `Nothing`."""
+
+    some = rm.Some(1)
+    assert some.and_then(lambda v: rm.Some(v * 2)) == rm.Some(2)
+    assert some.and_then(lambda _: rm.Nothing) == rm.Nothing
+
+    assert rm.Nothing.and_then(lambda v: rm.Some(v + 1)) == rm.Nothing
+    assert rm.Nothing.and_then(lambda _: rm.Nothing) == rm.Nothing
